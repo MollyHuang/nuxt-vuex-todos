773d6fdb3600e99d717236e958dae5ffe23a9b04
Molly Huang <molly.h@gmail.com>
2022-03-21 20:16:16 +0800

    Simple nuxt usage 1. store/index.js & page/index.vue 2. store/todos.js & page/todos.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <Tutorial/>
+  <!-- <Tutorial/> -->
+  <div>
+    <h1>{{ counter }}</h1>
+    <button type="button" @click="clickBtn">ADD</button>
+    <hr />
+    <h1>{{ asyncCount }}</h1>
+    <nuxt-link to="/todos">TODO List</nuxt-link>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  },
+  methods: {
+    clickBtn() {
+      console.log('click ...')
+      this.$store.commit('increment')
+    }
+  },
+  asyncData(context) {
+    return { asyncCount: context.app.store.state.counter };
+  },
 }
 </script>
